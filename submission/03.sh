d74eaa41972196f6d5ed5ce108657318984457ee
Vincent Mutwiri <vincentmutwiri9@gmail.com>
2026-03-25 14:04:18 +0300

    Update 03.sh

--- submission/03.sh
+++ submission/03.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 bitcoin-cli -regtest loadwallet "btrustwallet" >/dev/null 2>&1 || true
 ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
 
-# Mine 1 block to the address to fund it (regtest).
+
 bitcoin-cli -regtest generatetoaddress 1 "$ADDR" >/dev/null
 
 echo "$ADDR"
